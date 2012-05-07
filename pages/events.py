985459859c5f829ee1c5b3bd72656ea0c19f7a76
Rebecca Billings <rerlin@gmail.com>
2012-05-07 16:16:24 -0700

    Added Events page test

---
+++ pages/events.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from base import BasePage
+
+
+class EventsPage(BasePage):
+
+    _page_title = u'Events | QMO \u2013 quality.mozilla.org'
+
+    def go_to_events_page(self):
+        self.selenium.get(self.testsetup.base_url + '/events')
+        self.is_the_current_page
+
+    @property
+    def is_page_title_correct(self):
+        return self.selenium.get_title() == self._page_title
