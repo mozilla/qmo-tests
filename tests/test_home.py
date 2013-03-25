16f1923a0dad54a86c740418e04a771e55ddcd3b
Sasha Kruglov <sashakruglov@hotmail.com>
2013-03-26 01:13:03 +0400

    test for 404 error page

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,6 +10,7 @@ import requests
 from unittestzero import Assert
 
 from pages.home import HomePage
+from pages.not_found import NotFoundPage
 
 
 class TestHomePage:
@@ -36,3 +38,23 @@ class TestHomePage:
         Assert.equal(
             r.status_code, 200,
             u'request to %s responded with %s status code' % (robots_url, r.status_code))
+
+    @pytest.mark.nondestructive
+    def test_that_proper_404_error_page_displayed(self, mozwebqa):
+
+        not_found_page = NotFoundPage(mozwebqa)
+        not_found_page.go_to_not_found_page()
+
+        Assert.equal(not_found_page.page_title, u'Sorry, we couldn’t find that')
+
+        Assert.equal(not_found_page.get_page_status_code(), 404,
+                     u'GET request to this page should return 404 status code')
+
+        err_msg_parts = []
+        err_msg_parts.append(u'We looked everywhere, but we couldn’t find the page or file you were looking for. A few possible explanations:')
+        err_msg_parts.append(u'You may have followed an out-dated link or bookmark.')
+        err_msg_parts.append(u'If you entered the address by hand, you might have mistyped it.')
+        err_msg_parts.append(u'Maybe you found a bug. Good work!')
+        error_message = '\n'.join(err_msg_parts)
+
+        Assert.equal(not_found_page.error_message, error_message)
