c780fd169c5c72f9f46026d65a4d00cd497f526c
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-04-10 15:50:06 +0300

    test links on envents_page

--- tests/test_events.py
+++ tests/test_events.py
@@ -7,6 +7,7 @@
 import pytest
 from unittestzero import Assert
 
+from pages.link_crawler import LinkCrawler
 from pages.home import HomePage
 
 
@@ -18,3 +19,21 @@ class TestEventsPage:
         home_page.go_to_home_page()
         events_page = home_page.header_region.click_events_link()
         Assert.true(events_page.is_the_current_page)
+
+    @pytest.mark.nondestructive
+    def test_events_page_link(self, mozwebqa):
+        crawler = LinkCrawler(mozwebqa)
+        urls = crawler.collect_links('/category/events', id='content')
+        bad_urls = []
+
+        Assert.greater(
+            len(urls), 0, u'something went wrong. no links found.')
+
+        for url in urls:
+            check_result = crawler.verify_status_code_is_ok(url)
+            if check_result is not True:
+                bad_urls.append(check_result)
+
+        Assert.equal(
+            0, len(bad_urls),
+            u'%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
